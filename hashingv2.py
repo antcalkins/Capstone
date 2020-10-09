881c0047a2b1a200030fb6e85f34073407d06bb4
writer <lyall.rogers@gmail.com>
2020-10-09 01:34:48 -0400

    the beginnings of our database with comments

--- hashingv2.py
+++ hashingv2.py
@@ -1,22 +1,27 @@
+""" This code is designed to hash files and create a pandas dataframe that can be referenced by a graphical interface
+and modified by the user.
+Authors: L.E. Rogers and A.B. Calkins
+Last Edited: 09/10/2020
+"""
 import hashlib
 import glob
 import pandas as pd
 
-target = "/home/writer/"
-names = glob.glob(target + '**/*.*', recursive=True)
-hashed_dict = {}
-
+target = "/home/writer/"  # this sets the target directory
+names = glob.glob(target + '**/*.*', recursive=True)  # this goes through every file path inside of the directory
+hashed_dict = {}  # this is where the hashed files will be stored
 
 for i in range(0, len(names)):
+    # this creates the entries for the dictionary with the index being the full paths and the column info being hashes
     hashed_dict[names[i]] = str(hashlib.md5(names[i].encode('utf-8')).hexdigest()) + " " + \
                             str(hashlib.sha256(names[i].encode('utf-8')).hexdigest()) + " " + \
                             str(hashlib.sha1(names[i].encode('utf-8')).hexdigest())
 
-dataframe = pd.DataFrame(list(hashed_dict.items()), columns=['file names', 'hashes'])
-dataframe[['md5', 'sha256', 'sha1']] = dataframe['hashes'].str.split(expand=True)
-dataframe.drop(['hashes'], axis=1, inplace=True)
+dataframe = pd.DataFrame(list(hashed_dict.items()), columns=['full file paths', 'hashes'])  # creates dataframe
+dataframe[['md5', 'sha256', 'sha1']] = dataframe['hashes'].str.split(expand=True)  # splits hashes column by type
+dataframe.drop(['hashes'], axis=1, inplace=True)  # drops the unnecessary hashes column
 print(dataframe.info())
 print(dataframe.head())
-dataframe.to_csv("dataframe.csv")
+dataframe.to_csv("dataframe.csv")  # converts dataframe to a csv file
 
 
